76c6cc93d665dbf6fde5b445ff1a06864fe1c314
pushkaranand0312 <pushkaranand0312@gmail.com>
2018-08-16 18:27:08 +0530

    first commit

---
+++ getVMcount.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+numbers=`ssh -oStrictHostKeyChecking=no -oBatchMode=yes $1 sudo virsh list --all | grep -v Id | grep -v ^$| wc -l`
+
+if [ $numbers -ne 0 ]; then 
+   numbers=$(($numbers - 1))
+   echo "KVM :$1: $numbers"
+else 
+ number=`ssh -oStrictHostKeyChecking=no -oBatchMode=yes $1 sudo vzlist -a | grep -iv hostname | grep -v ^$| wc -l`
+ echo "VZ :$1 :$number"
+fi
